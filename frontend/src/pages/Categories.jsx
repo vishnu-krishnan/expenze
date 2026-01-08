d66252c92a058d2e1080c7c45baeaa29d748b03e
vishnu-krishnan <vishnuoffcl@gmail.com>
2026-01-08 11:21:45 +0530

    fixed bugs in UI

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
+import { getApiUrl } from '../utils/apiConfig';
 import ConfirmModal from '../components/ConfirmModal';
 import {
     Tag,
